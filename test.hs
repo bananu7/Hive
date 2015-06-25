8a9404a038419d0f76cfebb3f6c310dd4d2716c3
bananu7 <bananu7@o2.pl>
2015-06-25 21:04:49 +0200

    Added real tests

--- test.hs
+++ test.hs
@@ -1,10 +1,12 @@
 import Hive
 
 import Test.Hspec
-import Test.QuickCheck
+import qualified Data.Map.Strict as Map
 
 oneUnitMap = Map.insert (3,3) (PlayerWhite, Queen) emptyBoard
     where emptyBoard = Map.fromList [] :: Board
+
+twoUnitMap = Map.insert(3,4) (PlayerBlack, Queen) oneUnitMap
 
 players = [PlayerWhite, PlayerBlack]
 
@@ -13,18 +15,10 @@ main = hspec $ do
     describe "Hive's move logic" $ do
         it "prevents placing the unit on top of another unit" $ do
             isValidMove (MoveInsert (3,3) (PlayerBlack, Spider)) oneUnitMap `shouldBe` False
-            isValidMove (MoveInsert (3,3) (PlayerBlack, Beetle)) oneUnitMap `shouldBe` False
-
-main = do
-    let b = Map.fromList [] :: Board
-    let c = Map.insert (3,3) (PlayerWhite, Queen) b
-
-    test $ isValidMove (MoveInsert (3,2) (PlayerWhite, Spider)) c
-    test $ not $ isValidMove (MoveInsert (3,2) (PlayerBlack, Spider)) c
-    test $ not $ isValidMove (MoveInsert (3,3) (PlayerWhite, Spider)) c
-    test $ not $ isValidMove (MoveInsert (3,3) (PlayerBlack, Spider)) c
-    test $ not $ isValidMove (MoveInsert (3,2) (PlayerWhite, Queen)) c
-
-    where
-        test True = print "OK"
-        test False = print "FAIL"
+            isValidMove (MoveInsert (3,3) (PlayerBlack, Beetle Nothing)) oneUnitMap `shouldBe` False
+        it "requires a neighbouring ally" $ do
+            isValidMove (MoveInsert (3,2) (PlayerWhite, Spider)) oneUnitMap `shouldBe` True
+            isValidMove (MoveInsert (3,1) (PlayerWhite, Spider)) oneUnitMap `shouldBe` False
+        it "requires no neighbouring enemy" $ do
+            isValidMove (MoveInsert (2,3) (PlayerWhite, Spider)) twoUnitMap `shouldBe` True
+            isValidMove (MoveInsert (3,2) (PlayerWhite, Spider)) oneUnitMap `shouldBe` False
