5b966256a5475a203647cfb8686adef057daaa8b
maque <marcinmakowski@konto.pl>
2015-08-16 14:39:03 +0200

    Fix unit tests checking valid moves for neighbouring enemy
    Comment with ODD-Q hexboard placed
    twoUnitMap should be used for validation

--- test.hs
+++ test.hs
@@ -7,6 +7,21 @@ import qualified Data.Map.Strict as Map
 c :: (Int, Int) -> AxialCoord
 c = toAxial . OffsetCoord
 
+{--ODD-Q
+
+             /2,0\
+             \___/ /3,0\
+             /2,1\ \___/
+             \___/ /3,1\
+             /2,2 \\___/
+             \___ /3,2\
+             /2,3 \___/
+             \___//3,3\
+             /2,4\\WQ_/
+             \___ /3,4\
+                  \BQ_/
+--}
+
 oneUnitMap = insert (c (3, 3)) (PlayerWhite, Queen) emptyBoard
 twoUnitMap = insert (c (3, 4)) (PlayerBlack, Queen) oneUnitMap
 
@@ -23,4 +38,4 @@ main = hspec $ do
             isValidMove (MoveInsert (c (3, 1)) (PlayerWhite, Spider)) oneUnitMap `shouldBe` False
         it "requires no neighbouring enemy" $ do
             isValidMove (MoveInsert (c (2, 3)) (PlayerWhite, Spider)) twoUnitMap `shouldBe` True
-            isValidMove (MoveInsert (c (3, 5)) (PlayerWhite, Spider)) oneUnitMap `shouldBe` False
+            isValidMove (MoveInsert (c (2, 4)) (PlayerWhite, Spider)) twoUnitMap `shouldBe` False
