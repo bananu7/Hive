59b80fd2549e8dec5d311f58c6531412b8509d44
Bartek Banachewicz <bananu7@o2.pl>
2015-08-16 14:44:53 +0200

    Added black tiles

--- Main.hs
+++ Main.hs
@@ -18,26 +18,31 @@ import qualified Data.Map.Strict as Map
 -- sample 4
 
 data SampleState = SampleState {
-    _sprites :: [(Unit, Sprite)],
+    _sprites :: [((Player, Unit), Sprite)],
     _board :: Board
 }
 makeLenses ''SampleState
 
-imageData :: [(Unit, String)]
+imageData :: [((Player, Unit), String)]
 imageData = [
-    (Queen, "queen.png"),
-    (Spider, "spider.png"),
-    (Grasshopper, "hopper.png"),
-    (Beetle Nothing, "beetle.png"),
-    (Ant, "ant.png")
+    ((PlayerWhite, Queen), "queen.png"),
+    ((PlayerWhite, Spider), "spider.png"),
+    ((PlayerWhite, Grasshopper), "hopper.png"),
+    ((PlayerWhite, Beetle Nothing), "beetle.png"),
+    ((PlayerWhite, Ant), "ant_b.png"),
+    ((PlayerBlack, Queen), "queen_b.png"),
+    ((PlayerBlack, Spider), "spider_b.png"),
+    ((PlayerBlack, Grasshopper), "hopper_b.png"),
+    ((PlayerBlack, Beetle Nothing), "beetle_b.png"),
+    ((PlayerBlack, Ant), "ant_b.png")
     ]
 
-loadSprites :: IO [(Unit, Sprite)]
+loadSprites :: IO [((Player, Unit), Sprite)]
 loadSprites = do
     let withPaths = map (Control.Lens._2 %~ ("img/" ++)) imageData
     mapM turnIntoSprite withPaths
     where
-        turnIntoSprite :: (Unit, String) -> IO (Unit, Sprite)
+        turnIntoSprite :: ((Player, Unit), String) -> IO ((Player, Unit), Sprite)
         turnIntoSprite (a, b) = do
             b' <- loadSprite b
             return (a,b')
@@ -47,23 +52,25 @@ sampleLoad = SampleState <$> loadSprites
                          <*> pure startBoard
 
 startBoard = emptyBoard
-    & insert (OffsetCoord (1, 0)) (PlayerBlack, Spider)
-    & insert (OffsetCoord (2, 0)) (PlayerBlack, Queen)
-    & insert (OffsetCoord (3, 0)) (PlayerBlack, Ant)
-    & insert (OffsetCoord (1, 1)) (PlayerBlack, Ant)
-    & insert (OffsetCoord (1, 2)) (PlayerBlack, Ant)
-    
+    & insert (OffsetCoord (3, 3)) (PlayerWhite, Queen)
+    & insert (OffsetCoord (3, 4)) (PlayerBlack, Queen)
+    & insert (OffsetCoord (3, 2)) (PlayerBlack, Ant)
+    & insert (OffsetCoord (3, 2)) (PlayerBlack, Ant)
 
+displayScale :: Float
+displayScale = 0.6
+
+coordToPixel :: Coord c => c -> Vec2
 coordToPixel pos = Vec2 (fromIntegral x) (fromIntegral y)
-    where (x,y) = toPixel 200 pos
+    where (x,y) = toPixel (floor $ 200 * displayScale) pos
 
 sampleDraw :: DrawFn SampleState
 sampleDraw s = map drawPiece $ toList (s ^. board)
     where
-        drawPiece (pos, (_, unit)) = translate (coordToPixel pos) $ sprite TopLeft (findSprite unit)
+        drawPiece (pos, (owner, unit)) = scaled (Vec2 displayScale displayScale) $ translate (coordToPixel pos) $ sprite TopLeft (findSprite (owner, unit))
 
-        findSprite :: Unit -> Sprite
-        findSprite u = fromJust $ List.lookup u (s ^. sprites)
+        findSprite :: (Player, Unit) -> Sprite
+        findSprite pu = fromJust $ List.lookup pu (s ^. sprites)
 
 
 sampleUpdate :: UpdateFn SampleState
